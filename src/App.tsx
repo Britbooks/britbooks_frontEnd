269e4f23ea4112cce63cec43de491c748a47b9b1
Orizu <orizu1996@gmail.com>
2026-03-02 22:45:17 +0100

    new

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ import PrivacyPolicyPage from './pages/PrivacyPolicy';
 import { AuthProvider } from './context/authContext'; 
 import { CartProvider } from './context/cartContext';
 import { RecentlyViewedProvider } from './context/viewManager';
+import { WishlistProvider } from './context/wishlistContext';
 
 // ScrollToTop component
 const ScrollToTop: React.FC = () => {
@@ -47,39 +48,41 @@ const App: React.FC = () => {
     <AuthProvider>
       <CartProvider>
         <RecentlyViewedProvider>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/explore" element={<ExplorePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/order/:id" element={<OrdersPage />} />
-            <Route path="/item/:orderId/:itemIndex" element={<OrdersPage />} />
-            <Route path="/category" element={<CategoryBrowsePage />} />
-            <Route path="/browse/:id" element={<BrowseCategoryDetail />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/sustainability" element={<SustainabilityPage />} />
-            <Route path="/careers" element={<CareersPage />} />
-            <Route path="/press" element={<PressPage />} />
-            <Route path="/new-arrivals" element={<NewArrivalsPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/faq" element={<FAQPage />} />
-            <Route path="/shipping-returns" element={<ShippingReturnsPage />} />
-            <Route path="/bestsellers" element={<BestsellersPage />} />
-            <Route path="/special-offers" element={<SpecialOffersPage />} />
-            <Route path="/help" element={<HelpAndSupportPage />} />
-            <Route path="/checkout" element={<CheckoutFlow />} />
-            <Route path="/wishlist" element={<MyWishlistPage />} />
-            <Route path="/addresses" element={<AddressesPage />} />
-            <Route path="/settings" element={<AccountSettingsPage />} />
-            <Route path="/popular-books" element={<PopularBooksPage />} />
-            <Route path="/clearance" element={<ClearancePage />} />
-            <Route path="/return-policy" element={<ReturnPolicyPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="*" element={<div>Page not found</div>} />
-          </Routes>
+          <WishlistProvider>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/explore" element={<ExplorePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/orders" element={<OrdersPage />} />
+              <Route path="/order/:id" element={<OrdersPage />} />
+              <Route path="/item/:orderId/:itemIndex" element={<OrdersPage />} />
+              <Route path="/category" element={<CategoryBrowsePage />} />
+              <Route path="/browse/:id" element={<BrowseCategoryDetail />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/sustainability" element={<SustainabilityPage />} />
+              <Route path="/careers" element={<CareersPage />} />
+              <Route path="/press" element={<PressPage />} />
+              <Route path="/new-arrivals" element={<NewArrivalsPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/faq" element={<FAQPage />} />
+              <Route path="/shipping-returns" element={<ShippingReturnsPage />} />
+              <Route path="/bestsellers" element={<BestsellersPage />} />
+              <Route path="/special-offers" element={<SpecialOffersPage />} />
+              <Route path="/help" element={<HelpAndSupportPage />} />
+              <Route path="/checkout" element={<CheckoutFlow />} />
+              <Route path="/wishlist" element={<MyWishlistPage />} />
+              <Route path="/addresses" element={<AddressesPage />} />
+              <Route path="/settings" element={<AccountSettingsPage />} />
+              <Route path="/popular-books" element={<PopularBooksPage />} />
+              <Route path="/clearance" element={<ClearancePage />} />
+              <Route path="/return-policy" element={<ReturnPolicyPage />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+              <Route path="*" element={<div>Page not found</div>} />
+            </Routes>
+          </WishlistProvider>
         </RecentlyViewedProvider>
       </CartProvider>
     </AuthProvider>
